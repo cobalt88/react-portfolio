6102a3f55036cc5400b21df66f200fae5165e06a
Vincent Teune <50896378+cobalt88@users.noreply.github.com>
2022-08-16 21:31:02 -0400

    8.16.22
    remove project data prop

--- src/Components/FeaturedCards/projectData.js
+++ src/Components/FeaturedCards/projectData.js
@@ -1,31 +1,3 @@
-const projects = [
-  {
-    title: "The Rabbit Hole",
-    image: "image/logo URL goes here",
-    summary:
-      "Blog/Social Media platform aimed at sharing your learning journey.",
-    url: "https://therabbithole-blog.herokuapp.com",
-    repository: "repository link goes here",
-    technologies: "what did yu use to build it",
-  },
-  {
-    title: "Devise",
-    image: "image/logo URL goes here",
-    summary: "summary data goes here",
-    url: "deployment url goes here",
-    repository: "repository link goes here",
-    technologies: "what did yu use to build it",
-  },
-  {
-    title: "RecipEZ",
-    image: "image/logo URL goes here",
-    summary: "summary data goes here",
-    url: "deployment url goes here",
-    repository: "repository link goes here",
-    technologies: "what did yu use to build it",
-  },
-];
-
 /*  
   {
     title: "Title goes here",
