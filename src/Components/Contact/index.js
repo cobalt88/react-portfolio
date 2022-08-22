97cfc25ad63cb9e873b0f3d62b5f6441c4a5f276
Vincent Teune <50896378+cobalt88@users.noreply.github.com>
2022-08-21 21:59:47 -0400

    8.21.22
    update contact cards

--- src/Components/Contact/index.js
+++ src/Components/Contact/index.js
@@ -10,25 +10,35 @@ function Contact() {
 
       <Card className="contact-card">
         <h3 className="card-header">GitHub</h3>
-        <p className="card-body">Put Stuff Here</p>
-        <p className="card-footer">Maybe put something here?</p>
+        <p className="card-body">
+          <a href="https://github.com/cobalt88">Profile</a>
+          <br />
+          <a href="https://github.com/cobalt88?tab=repositories">
+            Repositories
+          </a>
+          <br />
+          <a href="https://gist.github.com/cobalt88">Gists</a>
+        </p>
       </Card>
 
       <Card className="contact-card">
         <h3 className="card-header">Reach Out</h3>
-        <p className="card-body">Put Stuff Here</p>
-        <p className="card-footer">Maybe put something here?</p>
+        <p className="card-body">
+          <a href="mailto:vincent@vtportfolio.net">vincent@vtportfolio.net</a>
+          <br />
+          Phone: 407-614-7677{" "}
+        </p>
       </Card>
 
       <Card className="contact-card">
         <h3 className="card-header">Request Form</h3>
-        <p className="card-body">Put Stuff Here</p>
+        <p className="card-body">Add link to request form here</p>
         <p className="card-footer">Maybe put something here?</p>
       </Card>
 
       <Card className="contact-card">
         <h3 className="card-header">Resume</h3>
-        <p className="card-body">Put Stuff Here</p>
+        <p className="card-body">Add link to resume here</p>
         <p className="card-footer">Maybe put something here?</p>
       </Card>
 
